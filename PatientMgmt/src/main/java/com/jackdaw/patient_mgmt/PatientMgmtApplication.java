09f8f52b10a3a571938f061d7d252ad83357d0ef
pratham <johnthapa13@gmail.com>
2025-05-09 18:59:06 +0545

    Project Setup

---
+++ PatientMgmt/src/main/java/com/jackdaw/patient_mgmt/PatientMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.jackdaw.patient_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMgmtApplication.class, args);
+	}
+
+}
---
+++ PatientMgmt/src/test/java/com/jackdaw/patient_mgmt/PatientMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackdaw.patient_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
